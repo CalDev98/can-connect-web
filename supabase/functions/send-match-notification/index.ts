aa8eb9a63eaed448527c5ab2693cc313a42e874a
Caldev <delicaleb609@gmail.com>
2025-12-01 22:40:40 +0100

    gestion de l'erreur de deno

--- supabase/functions/send-match-notification/index.ts
+++ supabase/functions/send-match-notification/index.ts
@@ -2,11 +2,13 @@
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
 
+
 // Setup type definitions for built-in Supabase Runtime APIs
 import "jsr:@supabase/functions-js/edge-runtime.d.ts"
 
 console.log("Hello from Functions!")
 
+// @ts-ignore
 Deno.serve(async (req) => {
   const { name } = await req.json()
   const data = {
