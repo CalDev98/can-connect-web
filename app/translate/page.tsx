47207bf3e22ccabc244ef022b49ea4bee83abc34
Caldev <delicaleb609@gmail.com>
2025-12-08 14:04:52 +0100

    remove speaker icon in translate page

--- app/translate/page.tsx
+++ app/translate/page.tsx
@@ -144,7 +144,7 @@ export default function TranslatePage() {
                     <h3 className="font-bold text-gray-700">{t("translate.phonetic")}</h3>
                     <div className="flex gap-2">
                       <Copy className="w-5 h-5 text-gray-400" />
-                      <Volume2 className="w-5 h-5 text-gray-400" />
+                      {/* <Volume2 className="w-5 h-5 text-gray-400" /> */}
                     </div>
                   </div>
                   <div className="flex items-center">
